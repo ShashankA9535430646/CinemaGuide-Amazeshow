0e143974a8a6348f72c5bc2e936c4c88fa35bad9
Shashank A <115521909+ShashankA9535430646@users.noreply.github.com>
2025-12-06 15:34:09 +0530

    updated url

---
+++ urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from app import views
+
+app_name = 'app'
+
+urlpatterns = [
+    path('register',views.register,name='register'),
+    path('login',views.login,name='login'),
+    path('logout',views.logout,name='logout'),
+    path('',views.moviess,name="movies"),
+    path('movies',views.moviess,name="index"),
+    path('tv shows',views.tvshowss,name="tv shows"),
+    path('movies+',views.addmovies,name="addmovies"),
+    path('tvshows+',views.addtvshows,name="addtvshows")
+]
+
+
